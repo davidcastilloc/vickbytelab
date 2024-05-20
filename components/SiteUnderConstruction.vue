fde3b7de0fe95187a6165aa5a0310efe96226594
vikruzdavid <vikruzdavid@gmail.com>
2024-05-20 09:24:41 +0000

    Initial Commit

---
+++ components/SiteUnderConstruction.vue
@@ -0,0 +1,19 @@
+<template>
+  <BaseCard class="p-6">
+    <BaseMessage 
+    icon 
+    rounded="md"> 
+    This site is under construction. 
+  </BaseMessage>
+  </BaseCard>
+</template>
+
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
+
